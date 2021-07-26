ef3a0bd35d88f46e8b6e368f670c6a78461bd05e
thecthebest <asyed5839@gmail.com>
2021-07-26 11:48:33 +0200

    Wrapped <Layout> around <MeetupList>
    <Layout> component takes "props.children" this allow any component to be wrapped by it.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import MeetupList from "../components/meetups/MeetupList";
+import Layout from "../components/layout/Layout";
 const Dummy_Meetup = [
     {id:"m1", title: "First Meetup", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Val_Trupchun.jpg/800px-Val_Trupchun.jpg", address:"switzerland", description:"drink"},
     {id:"m2", title: "First Meetup", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Lauterbrunnental_train.jpg/800px-Lauterbrunnental_train.jpg", address:"austria", description:"drink"},
@@ -7,7 +8,9 @@ const Dummy_Meetup = [
 function HomePage() {
     return (
         <Fragment>
-            <MeetupList meetups={Dummy_Meetup}/>
+            <Layout>
+                <MeetupList meetups={Dummy_Meetup}/>
+            </Layout>
         </Fragment>
     );
 }
