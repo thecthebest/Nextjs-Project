6a668d201b793f975971b33d18862c4b4076cb7e
thecthebest <asyed5839@gmail.com>
2021-07-26 12:02:46 +0200

    Wrapped <Component> by <Layout>
    As <Layout> is needed for more than one component, it is better to use in the root component which is managed bt Nextjs.

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import { Fragment } from "react";
 import MeetupList from "../components/meetups/MeetupList";
-import Layout from "../components/layout/Layout";
 const Dummy_Meetup = [
-    {id:"m1", title: "First Meetup", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Val_Trupchun.jpg/800px-Val_Trupchun.jpg", address:"switzerland", description:"drink"},
-    {id:"m2", title: "First Meetup", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Lauterbrunnental_train.jpg/800px-Lauterbrunnental_train.jpg", address:"austria", description:"drink"},
+    { id: "m1", title: "First Meetup", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Val_Trupchun.jpg/800px-Val_Trupchun.jpg", address: "switzerland", description: "drink" },
+    { id: "m2", title: "First Meetup", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Lauterbrunnental_train.jpg/800px-Lauterbrunnental_train.jpg", address: "austria", description: "drink" },
 ];
 function HomePage() {
     return (
         <Fragment>
-            <Layout>
-                <MeetupList meetups={Dummy_Meetup}/>
-            </Layout>
+            <MeetupList meetups={Dummy_Meetup} />
         </Fragment>
     );
 }
