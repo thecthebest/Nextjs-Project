37b092b8dcf93c03d48afbc156fa0342221ca283
thecthebest <asyed5839@gmail.com>
2021-07-27 15:13:26 +0200

    Added "getStaticProps" & "getStaticPaths" functions
    For displaying details about the meetups with a dynamic id. Both function are required, "getStaticPaths" returns an object with two properties. "fallback" has been set to "false" so that during the build process all pages for different ids are built. "getStaticProps" function takes "context" as a parameters and inside the function this give access to an object with various properties. Finally, this function also returns and object for meet up data.

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,5 +12,42 @@ function MeetupDetails() {
         />
     );
 }
+//This should be exported if a page is dynamic
+//getStaticProps is utilized
+export async function getStaticPaths() {
+    return {
+        //tells whetere your paths array contains all the supported parameters values or just some
+        //False:Contains all meetupId values and when something else is requested 404 is given
+        //True:Doesn't contain all meetupId values & will be generated when requested
+        fallback: false,
+        paths: [
+            //one object per verion of the [meetupId] page
+            {
+                params: {
+                    meetupId: "m1"
+                },
+            },
+            {
+                params: {
+                    meetupId: "m2"
+                },
+            },
+        ]
+    };
+}
+export async function getStaticProps(contetx) {
+    const meetupId = contetx.params.meetupId;
 
+    return {
+        props: {
+            meetupData: {
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Val_Trupchun.jpg/800px-Val_Trupchun.jpg",
+                id: meetupId,
+                title: "Starting point",
+                address: "Lucern, Swiss Alps",
+                description: "Amazing view"
+            }
+        }
+    };
+}
 export default MeetupDetails;
