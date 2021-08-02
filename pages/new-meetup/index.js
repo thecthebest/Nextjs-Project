f67684af2c971e4de7cb0ac4fb008a72e9b03876
thecthebest <asyed5839@gmail.com>
2021-08-02 16:08:17 +0200

    Sending data to new-meetup api endpoint
    When "addMeetupHandler" function is called a new post request is sent to "/api/new-meetup".

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,10 +1,23 @@
 // our-domain.com/new-meetup
 
+import { useRouter } from "next/dist/client/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetup() {
-    const addMeetupHandler = (eneteredMeetupData) => {
+    const addMeetupHandler = async (eneteredMeetupData) => {
         console.log(eneteredMeetupData);
+        //For local request to the server
+        const response = await fetch('/api/new-meetup',
+            {
+                method: 'POST',
+                body: JSON.stringify(eneteredMeetupData),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+        const data = await response.json();
+        console.log(data);
     };
     return (
         <NewMeetupForm onAddMeetup={addMeetupHandler} />
